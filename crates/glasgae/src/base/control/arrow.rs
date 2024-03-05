9565c3b4c8844f2a0624507baacb6371191384a5
Josh Palmer <1253239+Shfty@users.noreply.github.com>
2024-03-05 06:12:53 +0000

    Implement Category and Arrow

--- crates/glasgae/src/base/control/arrow.rs
+++ crates/glasgae/src/base/control/arrow.rs
@@ -1,4 +1,101 @@
-//! # Arrow
+//! Basic arrow definitions.
+//!
+//! Based on Generalising Monads to Arrows by John Hughes,
+//! Science of Computer Programming 37, pp67-111, May 2000.
+//!
+//! plus a couple of definitions (returnA and loop) from
+//!
+//! A New Notation for Arrows, by Ross Paterson,
+//! in ICFP 2001, Firenze, Italy, pp229-240.
+//!
+//! These papers and more information on arrows can be found at <http://www.haskell.org/arrows/>.
+//!
+/// Instances should satisfy the following laws:
+///
+/// ```text
+/// arr id = id
+/// ```
+/// ```text
+/// arr (f >>> g) = arr f >>> arr g
+/// ```
+/// ```text
+/// first (arr f) = arr (first f)
+/// ```
+/// ```text
+/// first (f >>> g) = first f >>> first g
+/// ```
+/// ```text
+/// first f >>> arr fst = arr fst >>> f
+/// ```
+/// ```text
+/// first f >>> arr (id *** g) = arr (id *** g) >>> first f
+/// ```
+/// ```text
+/// first (first f) >>> arr assoc = arr assoc >>> first f
+/// ```
+///
+/// where
+/// ```text
+/// assoc ((a,b),c) = (a,(b,c))
+/// ```
+///
+/// The other combinators have sensible default definitions, which may be overridden for efficiency.
+use crate::prelude::{Boxed, Function, FunctionT};
 
-// TODO
+/// Lift a function to an arrow.
+pub trait Arrow<A, B> {
+    fn arrow(self) -> Self;
+}
 
+impl<F, A, B> Arrow<A, B> for F
+where
+    F: FunctionT<A, B>,
+{
+    fn arrow(self) -> Self {
+        self
+    }
+}
+
+/// Split the input between the two argument arrows and combine their output. Note that this is in general not a functor.
+///
+/// The default definition may be overridden with a more efficient version if desired.
+pub trait Split<F, LA, LB, RA, RB> {
+    type Split;
+    fn split(self, other: F) -> Self::Split;
+}
+
+impl<FA, FB, LA, LB, RA, RB> Split<FB, LA, LB, RA, RB> for FA
+where
+    FA: Clone + FunctionT<LA, LB>,
+    FB: Clone + FunctionT<RA, RB>,
+{
+    type Split = Function<(LA, RA), (LB, RB)>;
+
+    fn split(self, g: FB) -> Self::Split {
+        let f = self;
+        (|(x, y)| (f(x), g(y))).boxed()
+    }
+}
+
+/// Send the input to both argument arrows and combine their output.
+///
+/// The default definition may be overridden with a more efficient version if desired.
+pub trait Fanout<F, LA, LB, RA, RB> {
+    type Fanout;
+    fn fanout(self, other: F) -> Self::Fanout;
+}
+
+impl<FA, FB, A, LB, RB> Fanout<FB, A, LB, A, RB> for FA
+where
+    FA: Clone + FunctionT<A, LB> + Split<FB, A, LB, A, RB>,
+    FB: Clone + FunctionT<A, RB>,
+    FA::Split: FunctionT<(A, A), (LB, RB)>,
+    A: Clone,
+{
+    type Fanout = Function<A, (LB, RB)>;
+
+    fn fanout(self, g: FB) -> Self::Fanout {
+        let f = self;
+        (|a: A| f.split(g)((a.clone(), a))).boxed()
+    }
+}
--- crates/glasgae/src/base/control/category.rs
+++ crates/glasgae/src/base/control/category.rs
@@ -1,3 +1,64 @@
-//! # Category
+//! A class for categories.
+//!
+//! Instances should satisfy the following laws:
+//!
+//! **Right identity**
+//! ```text
+//! f . id = f
+//! ```
+//!
+//! **Left identity**
+//! ```text
+//! id . f = f
+//! ```
+//!
+//! **Associativity**
+//! ```text
+//! f . (g . h) = (f . g) . h
+//! ```
 
-// TODO
+use std::convert::identity;
+
+use crate::prelude::{Boxed, Function, FunctionT};
+
+/// The identity morphism.
+pub trait Id<A, B> {
+    type Identity;
+    fn id() -> Self::Identity;
+}
+
+impl<F, A, B> Id<A, B> for F
+where
+    F: FunctionT<A, B>,
+    A: 'static,
+{
+    type Identity = Function<A, A>;
+
+    fn id() -> Self::Identity {
+        identity.boxed()
+    }
+}
+
+/// Utility alias to [`Id::Identity`].
+pub type IdentityT<T, A, B> = <T as Id<A, B>>::Identity;
+
+/// Morphism composition.
+pub trait Compose<FB, A, B, C> {
+    type Composed;
+    fn compose(self, m: FB) -> Self::Composed;
+}
+
+impl<FA, FB, A, B, C> Compose<FB, A, B, C> for FA
+where
+    FA: Clone + FunctionT<A, B>,
+    FB: Clone + FunctionT<B, C>,
+{
+    type Composed = Function<A, C>;
+
+    fn compose(self, m: FB) -> Self::Composed {
+        (|a| m(self(a))).boxed()
+    }
+}
+
+/// Utility alias to [`Compose::Composed`].
+pub type ComposedT<T, F, A, B, C> = <T as Compose<F, A, B, C>>::Composed;
