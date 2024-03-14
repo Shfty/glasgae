dd4297807deed4c3d947f95024714720276a9af9
Josh Palmer <1253239+Shfty@users.noreply.github.com>
2024-03-14 01:19:30 +0000

    Parse Pat instead of Ident in do chain

--- crates/glasgae-macros/src/do.rs
+++ crates/glasgae-macros/src/do.rs
@@ -2,14 +2,12 @@ use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
 use quote::{quote, ToTokens};
 use syn::parse::{Parse, ParseStream};
-use syn::{parse_macro_input, Expr, Ident, Local, Stmt, Token};
+use syn::{parse_macro_input, Expr, Local, Pat, Stmt, Token};
 
 use crate::punct;
 
-
-
 struct DoChain {
-    ident: Ident,
+    pat: Pat,
     _sep: punct::DoBind,
     expr: Expr,
     _semi: Token![;],
@@ -18,21 +16,18 @@ struct DoChain {
 impl Parse for DoChain {
     fn parse(input: ParseStream) -> syn::Result<Self> {
         let _input = input.fork();
-        let _ident: Ident = _input.parse()?;
+        let _pat: Pat = Pat::parse_single(&_input)?;
         let __sep: punct::DoBind = _input.parse()?;
         let _expr: Expr = _input.parse()?;
         let __semi: Token![;] = _input.parse()?;
 
-        let ident: Ident = input.parse().unwrap();
-
+        let pat: Pat = Pat::parse_single(input).unwrap();
         let _sep: punct::DoBind = input.parse().unwrap();
-
         let expr: Expr = input.parse().unwrap();
-
         let _semi: Token![;] = input.parse().unwrap();
 
         Ok(DoChain {
-            ident,
+            pat,
             _sep,
             expr,
             _semi,
@@ -89,31 +84,35 @@ impl Parse for DoBlock {
 
 impl ToTokens for DoBlock {
     fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
-        tokens.extend(self.0.iter().rfold(
-            TokenStream2::default(),
-            |acc: TokenStream2, next| match next {
-                DoTerm::Chain(DoChain { ident, expr, .. }) => quote! {
-                    glasgae::prelude::ChainM::chain_m(
-                        #expr,
-                        |#ident| #acc
-                    )
-                },
-                DoTerm::Let(local) => {
-                    quote!({#local #acc})
-                }
-                DoTerm::Then(expr) => {
-                    quote! {
-                        glasgae::prelude::ThenM::then_m(
-                            #expr, #acc
+        tokens.extend(
+            self.0.iter().rfold(
+                TokenStream2::default(),
+                |acc: TokenStream2, next| match next {
+                    DoTerm::Chain(DoChain {
+                        pat, expr, ..
+                    }) => quote! {
+                        glasgae::prelude::ChainM::chain_m(
+                            #expr,
+                            |#pat| #acc
                         )
+                    },
+                    DoTerm::Let(local) => {
+                        quote!({#local #acc})
                     }
-                }
-                DoTerm::Return(expr) => {
-                    assert!(acc.is_empty(), "Trailing input:\n{acc}");
-                    quote!(#expr)
-                }
-            },
-        ));
+                    DoTerm::Then(expr) => {
+                        quote! {
+                            glasgae::prelude::ThenM::then_m(
+                                #expr, #acc
+                            )
+                        }
+                    }
+                    DoTerm::Return(expr) => {
+                        assert!(acc.is_empty(), "Trailing input:\n{acc}");
+                        quote!(#expr)
+                    }
+                },
+            ),
+        );
     }
 }
 
@@ -121,4 +120,3 @@ pub fn r#impl(input: TokenStream) -> TokenStream {
     let block: DoBlock = parse_macro_input!(input);
     block.to_token_stream().into()
 }
-
