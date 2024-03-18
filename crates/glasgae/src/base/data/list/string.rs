c5435079d2d43abaf4d69884f2be2b01b39e8e55
Josh Palmer <1253239+Shfty@users.noreply.github.com>
2024-03-18 02:43:48 +0000

    Impl typeclasses for String

--- crates/glasgae/src/base/data/list/string.rs
+++ crates/glasgae/src/base/data/list/string.rs
@@ -4,6 +4,54 @@ impl Pointed for String {
     type Pointed = char;
 }
 
+impl WithPointed<char> for String {
+    type WithPointed = String;
+}
+
+impl Fmap<char> for String {
+    fn fmap(self, f: impl FunctionT<Self::Pointed, char>) -> Self::WithPointed {
+        self.chars().map(|t| f.to_function()(t)).collect()
+    }
+}
+
+impl Foldable<char> for String {
+    fn foldr(
+        self,
+        f: impl crate::base::data::function::bifunction::BifunT<Self::Pointed, char, char>,
+        z: char,
+    ) -> char {
+        self.chars().rfold(z, |acc, next| f.to_bifun()(next, acc))
+    }
+
+    fn foldl(
+        self,
+        f: impl crate::base::data::function::bifunction::BifunT<char, Self::Pointed, char>,
+        z: char,
+    ) -> char {
+        self.chars().fold(z, |acc, next| f.to_bifun()(acc, next))
+    }
+}
+
+impl Foldable1<char> for String {
+    fn foldr1(
+        self,
+        f: impl crate::base::data::function::bifunction::BifunT<char, char, char>,
+    ) -> char {
+        self.chars()
+            .reduce(|acc, next| f.to_bifun()(next, acc))
+            .unwrap_or_default()
+    }
+
+    fn foldl1(
+        self,
+        f: impl crate::base::data::function::bifunction::BifunT<char, char, char>,
+    ) -> char {
+        self.chars()
+            .reduce(|acc, next| f.to_bifun()(acc, next))
+            .unwrap_or_default()
+    }
+}
+
 impl Semigroup for String {
     fn assoc_s(self, a: Self) -> Self {
         self + &a
